137efe68d25e5a573b4380b6764b78bb943b5f4c
Konstantinos Peratinos <kotsosgr@gmail.com>
2018-03-17 19:24:08 +0100

    Change command to price, added parameters options

--- src/cryptkee.js
+++ src/cryptkee.js
@@ -63,31 +63,29 @@ bot.on('message', (message) => {
 
   let author = message.author;
 
-  if (command === 'psit') {
-    if (author.username === 'kotsoc - Gamaw') {
-    message.channel.send(`All hail Kotsoc the great Mastah! `);
-    } else {
-      message.channel.send(`Spread the Word
-        ${author}, Star Wars >>> Summoners War Suxx!`);
-    }
-  } else if (command === 'help') {
+ if (command === 'help') {
       const embedHelp = new Discord.RichEmbed().setTitle('Support')
-      .setColor(0x16689e).setDescription('Current version supports the top 10 cryptocurrencies in CoinMarketCap.\n Pricing information is in €.');
-      embedHelp.addField('Usage :', '``` $ [Name of Coin]\n```');
-      embedHelp.addField('Examples', '```$ BTC\n or\n$ ethereum```');
+      .setColor(0x16689e).setDescription('Current version supports the top 100 cryptocurrencies listed in CoinMarketCap.\n Pricing information is in €.');
+      embedHelp.addField('$List [n]', '``` Retrieves the list of top [n] cryptocurrencies\n```');
+      embedHelp.addField('$price [name]', '```Retrieves the price, 24h volume and change\n```');
+      embedHelp.addField('Examples', '```$price ethereum\nor\n$price BTC usd\n$list 10```');
+
       message.channel.send(embedHelp);
   } else if (command.includes('gam')) {
       message.channel.send(`Watch it ${author}!`);
-  } else if (supported.has(command) || shortNames.has(command)) {
+  } else if (command === 'price' && supported.has(args[0])
+   || shortNames.has(args[0])) {
       const https = require('https');
       let url;
       const embed = new Discord.RichEmbed();
 
-      if (command.length > 4) {
-        url = `https://api.coinmarketcap.com/v1/ticker/${command}/?convert=EUR`;
+      if (args[0].length > 3) {
+        //console.log(args[0] + shortNames.get(args[0]));
+        url = `https://api.coinmarketcap.com/v1/ticker/${args[0]}/?convert=EUR`;
       } else {
+        console.log(shortNames.get(args[0]));
         url = `https://api.coinmarketcap.com/v1/ticker/${shortNames
-          .get(command)}/?convert=EUR`;
+          .get(args[0])}/?convert=EUR`;
       }
       https.get(url, (resp) => {
       let data = '';
@@ -98,20 +96,40 @@ bot.on('message', (message) => {
 
       resp.on('end', () => {
         const reply = JSON.parse(data);
-        // console.log(reply);
-        embed.setAuthor(reply[0].name +' ('+ reply[0].symbol+')'
+        console.log(reply);
+        if (!reply.hasOwnProperty('error')) {
+          embed.setAuthor(reply[0].name +' ('+ reply[0].symbol+')'
           , 'https://s2.coinmarketcap.com/static/img/coins/16x16/1.png');
-        embed.setFooter('Prices via Coinmarketcap.com'
-        , 'https://coinmarketcap.com/favicon.ico');
-        embed.addField('Price', parseFloat(reply[0].price_eur)
-        .toFixed(3)+ ' €', true);
-        embed.addField('24h Volume', parseFloat(reply[0]['24h_volume_eur'])
-        .toFixed(3)+' €', true);
-        embed.addField('24h Change', parseFloat(reply[0].percent_change_24h)
-        .toFixed(3)+' %', true);
-        message.channel.send({embed});
+          embed.setFooter('Prices via Coinmarketcap.com'
+          , 'https://coinmarketcap.com/favicon.ico');
+          embed.addField('Price', parseFloat(reply[0].price_eur)
+          .toFixed(3)+ ' €', true);
+          embed.addField('24h Volume', parseFloat(reply[0]['24h_volume_eur'])
+          .toFixed(3)+' €', true);
+          embed.addField('24h Change', parseFloat(reply[0].percent_change_24h)
+          .toFixed(3)+' %', true);
+          message.channel.send({embed});
+        } else {
+          embed.addField('Error', 'Server could not find the currency')
+          message.channel.send({embed});
+        }
       });
       });
+  } else if (command === 'list') {
+      embed.setAuthor('Top 20 cryptocurrencies Today)'
+        , 'https://s2.coinmarketcap.com/static/img/coins/16x16/1.png');
+      for (let i = 0; i < supported.size; i++ ) {
+
+      }
+    } else if (command === 'psit') {
+      // Command used Debugging
+        if (author.username === 'kotsoc - Gamaw') {
+        message.channel.send(`All hail Kotsoc the great Mastah! `);
+        message.channel.send(`Args[${args}]`);
+        } else {
+          message.channel.send(`Spread the Word
+            ${author}, Star Wars >>> Summoners War Suxx!`);
+    }
   } else {
       message.channel.send(`I am not sure about that
         ${author}, but i know this, Summoners War Sux!`);
