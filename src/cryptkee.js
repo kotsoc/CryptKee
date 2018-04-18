1f9e66837aaae93716ae98486f59fc31c1a1b125
Konstantinos Peratinos <kotsosgr@gmail.com>
2018-04-18 16:05:58 +0200

    New Command + Updated help

--- src/cryptkee.js
+++ src/cryptkee.js
@@ -41,12 +41,12 @@ bot.on('ready', () => {
 
   resp.on('end', () => {
     const reply = JSON.parse(data);
-     console.log(reply[1].symbol + reply[1].id);
+     //console.log(reply[1].symbol + reply[1].id);
     for ( l = 0; l < reply.length; l++) {
       shortNames.set(reply[l].symbol.toLowerCase(), reply[l].id.toLowerCase())
       supported.add(reply[l].id.toLowerCase());
     }
-    console.log(shortNames.size);
+    //console.log(shortNames.size);
   });
   });
 });
@@ -65,14 +65,21 @@ bot.on('message', (message) => {
 
  if (command === 'help') {
       const embedHelp = new Discord.RichEmbed().setTitle('Support')
-      .setColor(0x16689e).setDescription('Current version supports the top 100 cryptocurrencies listed in CoinMarketCap.\n Pricing information is in €.');
-      embedHelp.addField('$List [n]', '``` Retrieves the list of top [10] cryptocurrencies\n```');
-      embedHelp.addField('$price [name]', '```Retrieves the price, 24h volume and change\n```');
-      embedHelp.addField('Examples', '```$price ethereum\nor\n$price BTC usd\n$list 10```');
+      .setColor(0x16689e).
+      setDescription('Current version supports the top 100 cryptocurrencies listed in CoinMarketCap.\n Pricing information is in €.');
+      embedHelp.
+      addField('$List [n]', '``` Retrieves the list of top [10] cryptocurrencies\n```');
+      embedHelp.
+      addField('$price [name]', '```Retrieves the price, 24h volume and change\n```');
+      embedHelp.
+      addField('$cap', '```Retrieves Global Data, Market cap, Bitcoin Percentage and others\n```');
+      embedHelp.
+      addField('Examples', '```$price ethereum\nor\n$price BTC usd\n$list 10```');
 
       message.channel.send(embedHelp);
-  } else if (command.includes('gam')) {
+  } else if (command.includes('fuc')) {
       message.channel.send(`Watch it ${author}!`);
+      // Pricing Data has to be Fresh
   } else if (command === 'price' && supported.has(args[0])
    || shortNames.has(args[0])) {
       const https = require('https');
@@ -80,10 +87,10 @@ bot.on('message', (message) => {
       const embed = new Discord.RichEmbed();
 
       if (args[0].length > 3) {
-        //console.log(args[0] + shortNames.get(args[0]));
+        // console.log(args[0] + shortNames.get(args[0]));
         url = `https://api.coinmarketcap.com/v1/ticker/${args[0]}/?convert=EUR`;
       } else {
-        console.log(shortNames.get(args[0]));
+        // console.log(shortNames.get(args[0]));
         url = `https://api.coinmarketcap.com/v1/ticker/${shortNames
           .get(args[0])}/?convert=EUR`;
       }
@@ -96,7 +103,7 @@ bot.on('message', (message) => {
 
       resp.on('end', () => {
         const reply = JSON.parse(data);
-        console.log(reply);
+        // console.log(reply);
         if (!reply.hasOwnProperty('error')) {
           embed.setAuthor(reply[0].name +' ('+ reply[0].symbol+')'
           , 'https://s2.coinmarketcap.com/static/img/coins/16x16/1.png');
@@ -115,7 +122,7 @@ bot.on('message', (message) => {
         }
       });
       });
-  } else if (command === 'list') {
+  }  else if (command === 'list') {
       const embed = new Discord.RichEmbed();
       let i = 0;
       let max = 10;
@@ -123,7 +130,7 @@ bot.on('message', (message) => {
       if (args[0] != null) {
         max = args[0];
       }
-      embed.setAuthor('Top 20 cryptocurrencies Today)'
+      embed.setAuthor('Top 20 cryptocurrencies Today'
         , 'https://s2.coinmarketcap.com/static/img/coins/16x16/1.png');
         embed.setFooter('List via Coinmarketcap.com'
         , 'https://coinmarketcap.com/favicon.ico');
@@ -133,9 +140,44 @@ bot.on('message', (message) => {
 
         }
         message.channel.send({embed});
+    } else if (command === 'cap' || command === 'volume') {
+        const https = require('https');
+        let url;
+        const embed = new Discord.RichEmbed();
+        url = `https://api.coinmarketcap.com/v1/global/?convert=EUR`;
+          // console.log(shortNames.get(args[0]));
 
+        https.get(url, (resp) => {
+        let data = '';
+
+      resp.on('data', (chunk) => {
+          data += chunk;
+        });
+
+      resp.on('end', () => {
+          const reply = JSON.parse(data);
+          // console.log(reply);
+          if (!reply.hasOwnProperty('error')) {
+            embed.setAuthor('Global Data', 'https://s2.coinmarketcap.com/static/img/coins/16x16/1.png');
+            embed.setFooter('Prices via Coinmarketcap.com'
+            , 'https://coinmarketcap.com/favicon.ico');
+            embed.addField('Total Market Cap', parseFloat(reply.total_market_cap_eur)
+            .toFixed(3)+ ' €', true);
+            embed.addField('24h Volume', parseFloat(reply.total_24h_volume_eur)
+            .toFixed(3)+' €', true);
+            embed.addField('Bitcoin Percentage', parseFloat(reply.bitcoin_percentage_of_market_cap)
+            .toFixed(3)+' %', true);
+            embed.addField('Active Currencies', parseFloat(reply.active_currencies)
+            .toFixed(3)+' %', true);
+            message.channel.send({embed});
+          } else {
+            embed.addField('Error', 'Server could not find the currency')
+            message.channel.send({embed});
+          }
+        });
+        });
     } else if (command === 'psit') {
-      // Command used Debugging
+      // Command used Debugging -- Only enabled for Author
         if (author.username === 'kotsoc - Gamaw') {
         message.channel.send(`All hail Kotsoc the great Mastah! `);
         message.channel.send(`Args[${args}]`);
